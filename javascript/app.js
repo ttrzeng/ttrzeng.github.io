6102cbfb957d116b0ff5637ccc8b14f28598bb50
ttrzeng <talent.z@Hotmail.com>
2017-06-16 17:52:21 -0400

    initial commit

---
+++ javascript/app.js
@@ -0,0 +1,68 @@
+var TalentPage = angular.module("TalentPage", ["ngMaterial", "ui.router", "ngRoute"]);
+
+TalentPage.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
+	function($stateProvider, $urlRouterProvider, $locationProvider){
+
+		$stateProvider
+			.state('blogPosts', {
+				url: '/',
+				templateUrl: 'posts.html'
+			})
+
+			.state('profile', {
+				url: '/profile',
+				templateUrl: 'profile.html'
+			})
+
+			.state('experience', {
+				url: '/experience',
+				templateUrl: 'experience.html'
+			})
+
+			.state('projects', {
+				url: '/projects',
+				templateUrl: 'projects.html'
+			})
+
+			.state('blog6', {
+				url: '/05-22-2017',
+				templateUrl: 'blogposts/05-22-2017.html'
+			})
+
+			.state('blog5', {
+				url: '/01-16-2017',
+				templateUrl: 'blogposts/01-06-2017.html'
+			})
+
+			.state('blog4', {
+				url: '/11-06-2016',
+				templateUrl: 'blogposts/11-06-2016.html'
+			})
+
+			.state('blog3', {
+				url: '/09-06-2016',
+				templateUrl: 'blogposts/09-06-2016.html'
+			})
+
+			.state('blog2', {
+				url: '/06-19-2016',
+				templateUrl: 'blogposts/06-19-2016.html'
+			})
+
+			.state('blog1', {
+				url: '/05-19-2016',
+				templateUrl: 'blogposts/05-19-2016.html'
+		});
+
+		$urlRouterProvider.otherwise('/');
+
+		// $locationProvider.html5Mode({
+		// 	enabled: true,
+		// 	requireBase: false
+		// });
+	}
+]);
+
+$(document).on("ready page:load", function(){
+	angular.bootstrap(document.body, ["TalentPage"]);
+});
