fff0a0bbd907988b458d30d5ccda29759dda11d6
ttrzeng <talent.z@Hotmail.com>
2017-07-04 10:31:42 -0400

    added new resume

--- javascript/app.js
+++ javascript/app.js
@@ -8,47 +8,38 @@ TalentPage.config(['$stateProvider', '$urlRouterProvider', '$locationProvider',
 				url: '/',
 				templateUrl: 'posts.html'
 			})
-
 			.state('profile', {
 				url: '/profile',
 				templateUrl: 'profile.html'
 			})
-
 			.state('experience', {
 				url: '/experience',
 				templateUrl: 'experience.html'
 			})
-
 			.state('projects', {
 				url: '/projects',
 				templateUrl: 'projects.html'
 			})
-
 			.state('blog6', {
 				url: '/05-22-2017',
 				templateUrl: 'blogposts/05-22-2017.html'
 			})
-
 			.state('blog5', {
 				url: '/01-16-2017',
 				templateUrl: 'blogposts/01-06-2017.html'
 			})
-
 			.state('blog4', {
 				url: '/11-06-2016',
 				templateUrl: 'blogposts/11-06-2016.html'
 			})
-
 			.state('blog3', {
 				url: '/09-06-2016',
 				templateUrl: 'blogposts/09-06-2016.html'
 			})
-
 			.state('blog2', {
 				url: '/06-19-2016',
 				templateUrl: 'blogposts/06-19-2016.html'
 			})
-
 			.state('blog1', {
 				url: '/05-19-2016',
 				templateUrl: 'blogposts/05-19-2016.html'
